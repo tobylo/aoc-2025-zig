f316f87f4ef65d55fb6bc9df65dea35c2ed15982
Tobias Lolax <tobias.lolax@gmail.com>
2025-11-30 02:51:24 +0100

    remove 2024 solutions

--- 3/data/data.zig
+++
@@ -1,6 +0,0 @@
-pub const test_input = @embedFile("test.txt");
-pub const input = @embedFile("input.txt");
-
-// Just in case we happen to need modified inputs for Part 2...
-pub const test_input2 = @embedFile("test2.txt");
-pub const input1 = @embedFile("input2.txt");
